5a5af873a7cd8b8db8ec741e7d3e4c63f51d3644
Alan Klement <alan.m.klement@gmail.com>
2014-08-12 20:53:08 -0400

    Revamped

--- app.js
+++ app.js
@@ -1,43 +1,55 @@
-var express = require('express'); 
-// var routes = require('./routes'); 
-var http = require('http');
+var express = require('express');
 var path = require('path');
-var config = require('./config');
-var compression = require('compression');
-var morgan = require('morgan');
-var errorhandler = require('errorhandler');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
+
 var app = express();
-// app.disable('x-powered-by');
-app.set('port', process.env.PORT || 8080);
-// app.set('title', 'alanklement.com');
-app.set('views', __dirname + config.VIEW_DIR);
-app.set('view engine', 'html');
-app.set('isProduction', process.env.NODE_ENV == 'production');
 
-app.engine('html', require('ejs').renderFile);
-// app.use(compression());
-// app.use(morgan());
-// app.use(errorhandler());
-// app.use(bodyParser());
-app.use(express.static(path.join(__dirname, config.VIEW_DIR)));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res){
-    res.redirect('/');
-  });
+app.use('/', routes);
 
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-function beginServer () {
-	http.createServer(app).listen(app.get('port'), function(){
+/// error handlers
 
-		if( app.get('isProduction')){
-			console.log("INIT ::: Production Express server listening on port " + app.get('port'));
-		}else{
-			console.log("INIT ::: _DEVELOPMENT_ Express server listening on port " + app.get('port'));
-		}
-	});
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
-beginServer();
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
