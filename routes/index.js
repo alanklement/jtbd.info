2146ed9b7be6e6cad9fe27f232c8e4f85e60f847
Alan Klement <alan.m.klement@gmail.com>
2014-12-17 12:37:17 -0500

    Spelling error

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+
+	if(req.query.v === "2"){
+  		res.render('index2', { title: 'Express' });
+	}else{
+  		res.render('index', { title: 'Express' });
+	}
+  
 });
 
 module.exports = router;
